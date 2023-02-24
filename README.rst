649766c325c645ab9424692266f7f6237becafa1
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-02-24 22:25:15 +0100

    chore: use pyproject.toml for packaging

---
+++ README.rst
@@ -0,0 +1,4 @@
+eSBAE (e-sbae)
+==============
+
+python module that facilitates the process of creating activity data for MRV reporting and much more.
--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-def parse_requirements(file):
-    return sorted(set(
-        line.partition('#')[0].strip()
-        for line in open(os.path.join(os.path.dirname(__file__), file))
-    )
-                  -set('')
-                  )
-
-setup(
-    name='eSBAE',
-    packages=find_packages(),
-    include_package_data=True,
-    version='0.1',
-    description='High-level functionality for the creation of high integrity MRV data',
-    install_requires=parse_requirements('requirements.txt'),
-    url='https://github.com/sepal-contrib/eSBAE',
-    author='Andreas Vollrath, Daniel Guerrero Machado, Pierrick Rambaud, Remi D\'Annunzio',
-    author_email='andreas.vollrath[at]fao.org',
-    license='MIT License',
-    keywords=['MRV', 'SBAE', 'Activity Data',
-              'REDD+', 'Remote Sensing', 'Time-series', 
-              'High Integrity'],
-    zip_safe=False #,
-    #setup_requires=['pytest-runner'],
-    #tests_require=['pytest']
-)
+setup()
