e1e02743528ea06c9309a872155681426e71c303
BuddyVolly <andreasvollrath@gmx.de>
2023-03-03 08:28:37 +0000

    adjusting dependencies

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,4 +1,16 @@
+from shapely.geometry import Polygon
+from e_sbae.sampling import random_point
+
+
+seed = np.random.seed(42)
 def test_tests() -> None:
     """test that the tests are running, to be replaced by actual tests"""
 
     assert True
+    
+    
+def test_random_point(geometry):
+    
+    retrieved_random_point = random_point(geometry)
+    assert retrieved_random_point == Point(12.5, 30)
+                            
