0a47994052b890d503bc7bc48c59528b115e5b96
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-03-20 10:25:28 +0100

    fix: replace retry by retrying

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,17 +1,10 @@
-from shapely.geometry import Polygon
-from e_sbae.sampling import random_point
-
+import e_sbae
+import numpy as np
 
 seed = np.random.seed(42)
+
 
 def test_tests() -> None:
     """test that the tests are running, to be replaced by actual tests"""
 
     assert True
-    
-    
-def test_random_point(geometry):
-    
-    retrieved_random_point = random_point(geometry)
-    assert retrieved_random_point == Point(12.5, 30)
-                            
